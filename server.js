2a6943197316844b7781d261ffdefc4c155b2d96
Satheesh Smart <vallurusateesh2009@gmail.com>
2015-08-26 08:35:24 +0530

    Removing Unnecessary dependencies

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express             = require('express'),
     app                 = express();
 var favicon             = require('serve-favicon');
-var bodyParser          = require('body-parser'); 
+var bodyParser          = require('body-parser');
 var bson                = require('bson');
 var mongoose            = require('mongoose');
 var dbName              = 'gameofthrones';
@@ -10,28 +10,30 @@ var env                 = process.env.NODE_ENV = process.env.NODE_ENV;
 Controllers
 ****************/
 var familyController    = require('./server/controller/familyController');
-/**************** 
+/****************
 Connecting to DB
 *****************/
 var dbUrl               = '';
 /*if(env == 'development'){
-dbUrl                   = 'mongodb://127.0.0.1:27017/'+dbName; 
+dbUrl                   = 'mongodb://127.0.0.1:27017/'+dbName;
 }else{
-dbUrl                   = 'mongodb://got:143143341@ds047672.mongolab.com:47672/'+dbName;   
-}*/
 dbUrl                   = 'mongodb://got:143143341@ds047672.mongolab.com:47672/'+dbName;
+}*/
+dbUrl                   = 'mongodb://127.0.0.1:27017/'+dbName;
+//dbUrl                   = 'mongodb://got:143143341@ds047672.mongolab.com:47672/'+dbName;
 mongoose.connect(dbUrl);
 var db                  = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
   console.log('Connected to DB @ '+dbUrl);
 });
-/**************** 
+/****************
 Activating Express Server
 *****************/
-var port                = process.env.PORT || 9090; 
+var port                = process.env.PORT || 9090;
+var host                = process.env.HOST || '127.0.0.1';
 var server              = app.listen(port, function () {
-  var host              = server.address().address;
+
   console.log('App listening at http://%s:%s', host, port);
 });
 app.use(express.static('client'));
